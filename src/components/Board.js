51d44d1ba1e9af99ebf6f3b950dde2ff4d93a0ef
Josh Talley <jtdev84@gmail.com>
2023-01-19 13:51:33 +0000

    Add calculateWinner function

--- src/components/Board.js
+++ src/components/Board.js
@@ -17,15 +17,6 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
-  // Steps of Board function:
-  // Initialising state for the squares on the board as an Array with a length of 9 values that are set to be 'null'
-  // Initialising another state to mark X and O on the board in order
-  // using slice method to copy the squares array, keeping the original state array immutable
-  // if statement that checks if a square is already filled and stops it from being changed
-  // Ternary operator that determines if X or O will fill the empty square that is clicked
-  // setSquare function is called with the copied array defined above
-  // setXIsNext function is called to flip the status of the boolean
-
   return (
     <>
       <div className="board-row">
@@ -45,4 +36,33 @@ export default function Board() {
       </div>
     </>
   );
-}
+};
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+};
+
+// Steps of Board function:
+  // Initialising state for the squares on the board as an Array with a length of 9 values that are set to be 'null'
+  // Initialising another state to mark X and O on the board in order
+  // using slice method to copy the squares array, keeping the original state array immutable
+  // if statement that checks if a square is already filled and stops it from being changed
+  // Ternary operator that determines if X or O will fill the empty square that is clicked
+  // setSquare function is called with the copied array defined above
+  // setXIsNext function is called to flip the status of the boolean
